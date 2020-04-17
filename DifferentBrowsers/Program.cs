bdf027492cfbd0711b5b02b45d6fdae25f585375
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-04-16 21:13:29 -0500

    DifferentBrowsers

---
+++ DifferentBrowsers/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+
+namespace DifferentBrowsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+
+            System.Console.WriteLine(driver.GetType().ToString());
+
+            if (driver.GetType().ToString().Contains("InternetExplorer"))
+                driver.Navigate().GoToUrl("https://www.hotmail.com/");
+            else if(driver.GetType().ToString().Contains("Chrome"))
+                driver.Navigate().GoToUrl("https://www.gmail.com/");
+
+
+        }
+    }
+}
