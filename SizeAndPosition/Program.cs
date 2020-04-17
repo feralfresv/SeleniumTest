9f6131d9da10b10a10c740677ec50a6f60d0eb19
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-04-16 21:31:51 -0500

    SizeAndPosition

---
+++ SizeAndPosition/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Drawing;
+
+namespace SizeAndPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Size size = new Size();
+            size.Width = 300;
+            size.Height = 300;
+
+            Point position = new Point();
+            position.X = 400;
+            position.Y = 250;
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Size = size;
+            driver.Manage().Window.Position = position;
+
+            driver.Navigate().GoToUrl("http://testing.todvachev.com");
+
+
+        }
+    }
+}
